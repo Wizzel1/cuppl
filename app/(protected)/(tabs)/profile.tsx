296a1a8e2b28b1db0fb9d4c4277ee331011c9c03
Wizzel1 <r_christian@gmx.de>
2025-03-22 16:38:56 +0100

    Add profile information

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -14,10 +14,18 @@ export default function Profile() {
   }, [couple?.id]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <Text>Profile</Text>
       <Button title="Log Out" onPress={logOut} />
       {couple ? <Text>Couple {couple.id}</Text> : <Text>No couple</Text>}
+      {couple ? <Text>Couple owner {couple._owner.id}</Text> : <Text>No couple owner</Text>}
+      <Text>My account {me.id}</Text>
+
       <Text selectable>{inviteLink}</Text>
     </View>
   );
