ce76041bba18b0bc4b5eec24e2cdd36216a2c200
Wizzel1 <r_christian@gmx.de>
2025-03-27 11:36:45 +0100

    Add shoppingList schema

--- src/schemas/shoppingSchema.ts
+++ src/schemas/shoppingSchema.ts
@@ -1,6 +1,16 @@
-import { co, CoMap } from 'jazz-tools';
+import { co, CoList, CoMap } from 'jazz-tools';
 
 class ShoppingItem extends CoMap {
   name = co.string;
   age = co.number;
+}
+
+export class ShoppingItems extends CoList.Of(co.ref(ShoppingItem)) {}
+export class ShoppingList extends CoMap {
+  title = co.string;
+  items = co.ref(ShoppingItems);
+  creatorAccID = co.string;
+  assignedTo = co.literal('me', 'partner', 'us');
+  deleted = co.boolean;
+  isHidden = co.boolean;
 }
