c5dbeaa4ab5a5ed48499b5dd2c143b3d84d9ef14
Wizzel1 <r_christian@gmx.de>
2025-03-27 13:33:31 +0100

    Add styling

--- app/(protected)/(tabs)/(lists)/shopping.tsx
+++ app/(protected)/(tabs)/(lists)/shopping.tsx
@@ -1,9 +1,96 @@
-import { Text, View } from 'react-native';
+import { useMemo } from 'react';
+import { SectionList, Text, View } from 'react-native';
+
+import FloatingActionButton from '~/components/FloatingActionButton';
+import { useCouple, usePartnerProfiles } from '~/src/schemas/schema.jazz';
+import { ShoppingList } from '~/src/schemas/shoppingSchema';
 
 export default function Shopping() {
+  const couple = useCouple();
+  const { myProfile, partnerProfile } = usePartnerProfiles();
+
+  const handlePress = () => {
+    console.log('handlePress');
+  };
+
+  const { myLists, partnerLists, sharedLists } = useMemo(() => {
+    const empty = { myLists: [], partnerLists: [], sharedLists: [] };
+    if (!couple?.shoppingLists) return empty;
+    if (!myProfile?.accountId || !partnerProfile?.accountId) return empty;
+    const myAccountId = myProfile.accountId;
+    const partnerAccountId = partnerProfile.accountId;
+
+    const myLists: ShoppingList[] = [];
+    const partnerLists: ShoppingList[] = [];
+    const sharedLists: ShoppingList[] = [];
+
+    for (const list of couple.shoppingLists) {
+      if (!list) continue;
+      if (list.deleted) continue;
+      switch (list.assignedTo) {
+        case 'me':
+          if (list.creatorAccID === myAccountId) myLists.push(list);
+          if (list.creatorAccID === partnerAccountId) partnerLists.push(list);
+          break;
+        case 'partner':
+          if (list.creatorAccID === myAccountId) partnerLists.push(list);
+          if (list.creatorAccID === partnerAccountId) myLists.push(list);
+          break;
+        case 'us':
+          sharedLists.push(list);
+          break;
+      }
+    }
+
+    return {
+      myLists,
+      partnerLists,
+      sharedLists,
+    };
+  }, [couple?.todoLists, myProfile?.accountId, partnerProfile?.accountId]);
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <Text>Shopping</Text>
+      <SectionList
+        sections={[
+          { title: 'My Lists', data: myLists },
+          { title: "Partner's Lists", data: partnerLists },
+          { title: 'Shared Lists', data: sharedLists },
+        ]}
+        renderSectionHeader={({ section }) => (
+          <Text
+            style={{
+              paddingHorizontal: 24,
+              paddingTop: 12,
+              paddingBottom: 12,
+              fontSize: 18,
+              color: '#18181B',
+              fontWeight: '600',
+              backgroundColor: 'white',
+            }}>
+            {section.title}
+          </Text>
+        )}
+        ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
+        renderItem={({ item }) => {
+          if (!item) return null;
+          return (
+            <Text>{item.title}</Text>
+            // <TodoListListItem
+            //   key={item.id}
+            //   title={item.title}
+            //   onPress={() => onItemPress(item.id)}
+            //   listId={item.id}
+            //   onDelete={() => {}}
+            //   onEdit={() => {
+            //     setToUpdate(item);
+            //     bottomSheetModalRef.current?.present();
+            //   }}
+            // />
+          );
+        }}
+      />
+
+      <FloatingActionButton onPress={handlePress} icon="add" color="#27272A" />
     </View>
   );
 }
