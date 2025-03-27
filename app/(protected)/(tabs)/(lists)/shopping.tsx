d6ea381ffadc7dca8924c02eb4d967369fbf22ac
Wizzel1 <r_christian@gmx.de>
2025-03-27 13:21:25 +0100

    Update styling

--- app/(protected)/(tabs)/(lists)/shopping.tsx
+++ app/(protected)/(tabs)/(lists)/shopping.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function Shopping() {
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
       <Text>Shopping</Text>
     </View>
   );
