885a6671528895e0df8772ee100b5b4a3e9d6f7f
Wizzel1 <r_christian@gmx.de>
2025-03-28 10:52:09 +0100

    Add shoppinglist list item

---
+++ components/ShoppingListScreen/ShoppingListListItem.tsx
@@ -0,0 +1,208 @@
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { ProgressiveImg, useCoState } from 'jazz-react-native';
+import { ID, ImageDefinition } from 'jazz-tools';
+import { memo, useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { GestureHandlerRootView, Pressable } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Reanimated, { SharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
+import { ShoppingList } from '~/src/schemas/shoppingSchema';
+
+type RightActionProps = {
+  onDelete: () => void;
+  onEdit: () => void;
+};
+function RightAction(
+  prog: SharedValue<number>,
+  drag: SharedValue<number>,
+  { onDelete, onEdit }: RightActionProps
+) {
+  const styleAnimation = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: drag.value + 130 }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <View style={styles.rightActionContainer}>
+        <Pressable onPress={onEdit} style={styles.rightActionButton}>
+          <Ionicons name="pencil" size={20} color="#000000" />
+        </Pressable>
+        <Pressable
+          onPress={onDelete}
+          style={{ ...styles.rightActionButton, backgroundColor: '#FFE5E5' }}>
+          <Ionicons name="trash-outline" size={20} color="#FF0000" />
+        </Pressable>
+      </View>
+    </Reanimated.View>
+  );
+}
+
+function ShoppingListListItem({
+  avatar,
+  title,
+  listId,
+  onPress,
+  onDelete,
+  onEdit,
+  disableSwipe = false,
+}: {
+  avatar?: ImageDefinition | null;
+  title: string;
+  listId: string;
+  onPress: () => void;
+  onDelete?: () => void;
+  onEdit?: () => void;
+  disableSwipe?: boolean;
+}) {
+  const list = useCoState(ShoppingList, listId as ID<ShoppingList>);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!list?.id) return;
+    if (isLoaded) return;
+    setIsLoaded(false);
+    list?.ensureLoaded({ items: [{}] }).then(() => {
+      setIsLoaded(true);
+    });
+  }, [list?.id]);
+
+  const completedItemsCount = list?.completedItems.length ?? 0;
+  const totalItemsCount = list?.liveItems.length ?? 0;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <GestureHandlerRootView>
+      <Swipeable
+        enabled={!disableSwipe}
+        friction={1.8}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        onSwipeableOpen={() => setIsOpen(true)}
+        renderRightActions={(progress, drag, swipeable) =>
+          RightAction(progress, drag, {
+            onDelete: () => {
+              setIsOpen(false);
+              swipeable.close();
+              onDelete?.();
+            },
+            onEdit: () => {
+              setIsOpen(false);
+              swipeable.close();
+              onEdit?.();
+            },
+          })
+        }>
+        <TouchableOpacity onPress={isOpen ? () => setIsOpen(false) : onPress}>
+          <View style={styles.container}>
+            <View style={styles.contentWrapper}>
+              {list?.backgroundColor && list?.emoji ? (
+                <View style={[styles.avatarContainer, { backgroundColor: list.backgroundColor }]}>
+                  <Text style={styles.emojiText}>{list.emoji}</Text>
+                </View>
+              ) : (
+                <ProgressiveImg image={avatar} maxWidth={400}>
+                  {({ src, originalSize, res }) => (
+                    <Image source={{ uri: src }} style={styles.avatarImage} />
+                  )}
+                </ProgressiveImg>
+              )}
+
+              <View style={styles.textContainer}>
+                <Text numberOfLines={1} ellipsizeMode="tail" style={styles.titleText}>
+                  {title}
+                </Text>
+                {isLoaded ? (
+                  <Text style={styles.subtitleText}>
+                    {completedItemsCount} / {totalItemsCount}
+                  </Text>
+                ) : (
+                  <Text style={styles.subtitleText}>Syncing with partner...</Text>
+                )}
+              </View>
+              {list?.isHidden && (
+                <MaterialCommunityIcons name="eye-off" size={20} color="#A1A1AA" />
+              )}
+            </View>
+            <Ionicons
+              name="chevron-forward-outline"
+              size={24}
+              color="black"
+              style={styles.chevron}
+            />
+          </View>
+        </TouchableOpacity>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  contentWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+    flex: 1,
+  },
+  avatarContainer: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emojiText: {
+    fontSize: 20,
+  },
+  avatarImage: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    resizeMode: 'cover',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#18181B',
+  },
+  subtitleText: {
+    fontSize: 14,
+    color: '#71717B',
+  },
+  chevron: {
+    paddingLeft: 18,
+  },
+  rightAction: { width: 75, height: 75, backgroundColor: 'purple' },
+  separator: {
+    width: '100%',
+    borderTopWidth: 1,
+  },
+  rightActionContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 130,
+    paddingRight: 24,
+  },
+  rightActionButton: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default memo(ShoppingListListItem);
