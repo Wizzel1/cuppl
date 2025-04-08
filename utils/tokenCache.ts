8f72ac719f14544cab5d42d91dc98a1502f153a3
Wizzel1 <r_christian@gmx.de>
2025-04-08 12:52:05 +0200

    Fix token cache

--- utils/tokenCache.ts
+++ utils/tokenCache.ts
@@ -1,6 +1,11 @@
-import { TokenCache } from '@clerk/clerk-expo/dist/cache';
 import * as SecureStore from 'expo-secure-store';
 import { Platform } from 'react-native';
+
+export interface TokenCache {
+  getToken: (key: string) => Promise<string | undefined | null>;
+  saveToken: (key: string, token: string) => Promise<void>;
+  clearToken: (key: string) => void;
+}
 
 const createTokenCache = (): TokenCache => {
   return {
@@ -22,8 +27,12 @@ const createTokenCache = (): TokenCache => {
     saveToken: (key: string, token: string) => {
       return SecureStore.setItemAsync(key, token);
     },
+    clearToken: (key: string) => {
+      return SecureStore.deleteItemAsync(key);
+    },
   };
 };
 
 // SecureStore is not supported on the web
+// https://github.com/expo/expo/issues/7744#issuecomment-611093485
 export const tokenCache = Platform.OS !== 'web' ? createTokenCache() : undefined;
